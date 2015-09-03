44d369c8e2cd3fe087bbd3b9c9f1fb387aa37609
Naoki Yokoyama <yokoyama.naoki.ye7@is.naist.jp>
2015-09-03 12:07:46 +0900

    プレーヤーが餌を捕獲するゲーム

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  transform.Rotate(new Vector3(0f,0.8f,0f)); // y軸を軸として90°回転
+
+	}
+}
